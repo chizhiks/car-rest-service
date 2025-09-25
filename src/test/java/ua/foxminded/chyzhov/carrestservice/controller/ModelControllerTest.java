455c6fcc79fdfc8bacd41ca78ed6d8eba7b664e3
Andrii Chyzhov <andreychizhov9@gmail.com>
2025-09-26 00:09:04 +0300

    Task 4.5 | Dockerization
    - Create docker image for car-rest-service app
    - Create docker-compose file to include all required infrastructure to run car-rest-service app
    - Additionally, modify several classes and tests to improve Docker compatibility

--- src/test/java/ua/foxminded/chyzhov/carrestservice/controller/ModelControllerTest.java
+++ src/test/java/ua/foxminded/chyzhov/carrestservice/controller/ModelControllerTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import ua.foxminded.chyzhov.carrestservice.config.TestSecurityConfig;
 import ua.foxminded.chyzhov.carrestservice.dto.MakeDto;
 import ua.foxminded.chyzhov.carrestservice.dto.ModelDto;
 import ua.foxminded.chyzhov.carrestservice.service.ModelService;
@@ -26,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ModelController.class)
+@Import(TestSecurityConfig.class)
 public class ModelControllerTest {
 
     @Autowired
